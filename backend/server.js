a54febfa8ccb56479cbe802c013e100aed4ec824
Aiman <aimantariq1201@gmail.com>
2026-07-15 21:24:03 +0500

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use("/api/events", eventRoutes);
 // 2. Naya Membership Route Register Karein
 app.use('/api/members', memberRoutes); 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
