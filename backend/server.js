cbbe90310f08d29f14ff4c9e1ecfa71682848029
Aiman <aimantariq1201@gmail.com>
2026-07-15 22:24:25 +0500

    modified db connection

--- backend/server.js
+++ backend/server.js
@@ -9,21 +9,17 @@ import memberRoutes from './routes/MemberRoutes.js';
 dotenv.config();
 
 // MySQL Connection
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+import mysql from "mysql2";
+
+export const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error("❌ DB Error:", err);
+    console.error("❌ Full DB Error:", err);
     process.exit(1);
-  } else {
-    console.log("✅ MySQL connected!");
   }
+
+  console.log("✅ MySQL connected!");
 });
 
 const app = express();
