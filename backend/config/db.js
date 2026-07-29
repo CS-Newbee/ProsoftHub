6b1f7225ab9b93c6844bd3717698291dcf4887aa
CS-Newbee <aimantariq1201@gmail.com>
2026-07-30 00:05:47 +0500

    Update DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql2';
+import mysql from "mysql2/promise";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -12,12 +12,16 @@ console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('==================\n');
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT) || 3307
+  port: Number(process.env.DB_PORT) || 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect((err) => {
